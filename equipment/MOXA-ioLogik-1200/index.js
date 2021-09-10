68980b99b5611d5d5c75f840036af31293bdd3e4
igor <work.gribov@gmail.com>
2021-09-10 15:36:10 +0300

    чистка кода, без изменения функциональности

--- equipment/MOXA-ioLogik-1200/index.js
+++ equipment/MOXA-ioLogik-1200/index.js
@@ -9,8 +9,8 @@ exports.run = function () {
     getJSON.getJSON(options, function (statusCode, result) {
         let now = new Date();
         let dirName = now.toISOString().substr(0, 10) +"/";
-        let fileName =now.toISOString().replace(/[:,\.]/g,'-') + ".json";
-        fs.stat(config.server.dir + dirName, function (err, stats) {
+        let fileName =now.toISOString().replace(/[:,.]/g,'-') + ".json";
+        fs.stat(config.server.dir + dirName, function (err) {
             if (err){
                 fs.mkdir(config.server.dir + dirName, function (){
                     fs.writeFile(config.server.dir + dirName + fileName, JSON.stringify(result), function (){});
