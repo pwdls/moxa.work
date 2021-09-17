4c1b1a0f7cc57a6d1eb1122d688462b1f0c92491
igor <work.gribov@gmail.com>
2021-09-17 13:50:58 +0300

    Заглушка для вывода authorization

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,21 +1,25 @@
-
 class apiController {
-    async work(req, res){
+    async work(req, res) {
 
         console.log(req.body);
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/json")
 
-        switch (req.body.request){
+        switch (req.body.request) {
             case "getConnect":
-                let result = {
+                res.send(JSON.stringify({
                     result: 0,
                     data: {
                         "typeEquipment": "MOXA-ioLogik-1200",
                         "userStatus": "Выключен"
                     }
-                }
-                res.send(JSON.stringify(result));
+                }));
+                break;
+            case "authorization":
+                res.send(JSON.stringify({
+                    result: 0,
+                    data: {}
+                }));
                 break;
             default:
                 res.send("{result:999}");
