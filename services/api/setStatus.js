57a81f9752f7a88794733a72817d13219ae492de
igor <work.gribov@gmail.com>
2021-09-21 20:16:25 +0300

    добавлено setStatus.js

---
+++ services/api/setStatus.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const fs = require("fs");
+
+class getStatus {
+
+    work(req, res) {
+        fs.writeFile(process.env.midl_path_userStatus, JSON.parse(req.body.data)["userStatus"], () => {
+            res.send(
+                JSON.stringify({
+                    result: 0,
+                    data: {
+                        "typeEquipment": process.env.midl_typeEquipment,
+                        "userStatus": JSON.parse(req.body.data)["userStatus"]
+                    }
+                })
+            );
+        });
+    }
+
+}
+
+module.exports = new getStatus();
