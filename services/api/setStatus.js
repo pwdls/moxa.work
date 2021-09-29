00d02a32e4cd4be5e345e5c3e38948c9e2a30ad6
igor <work.gribov@gmail.com>
2021-09-29 12:55:08 +0300

    исправил ошибку полкчения статуса при setStatus.js

--- services/api/setStatus.js
+++ services/api/setStatus.js
@@ -9,7 +9,7 @@ class getStatus {
                     result: 0,
                     data: {
                         "typeEquipment": process.env.midl_typeEquipment,
-                        "userStatus": req.body.data.userStatus
+                        "userStatus": JSON.parse(req.body.data).userStatus
                     }
                 })
             );
