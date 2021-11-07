d36f3bb5742301e4b258cd9115305ad26ca1b513
igor <work.gribov@gmail.com>
2021-11-07 22:34:17 +0300

    добавил функцию getOperating.js

---
+++ services/api/getOperating.js
@@ -0,0 +1,46 @@
+class getOperating {
+    work(req, res) {
+
+        let f = [
+            12.1, 4.6, 6.5, 3.3, 1.3,
+            16.2, 6.6, 3.6, 2.7, 1.6,
+            5.6, 3.6, 6.2, 1.6, 4.5,
+            21.2, 6.6, 7.6, 5.5, 2.9,
+            3.5, 1.6, 8.2, 3.4, 8.4,
+            2.1, 3.6, 5.6, 3.3, 6.3,
+            2.3, 7.3, 4.3, 2.1, 7.7,
+            21.2, 6.6, 7.6, 5.5, 2.9,
+            3.5, 1.6, 8.2, 3.4, 8.4,
+            2.1, 3.6, 5.6, 3.3, 6.3,
+            2.3, 7.3, 4.3, 2.1, 7.7,
+        ]
+        let date = new Date();
+        let day = date.getDate();
+        let hour = date.getHours();
+
+        let day1 = (new Date()).toISOString();
+        let day2 = (new Date(Date.now() - 86400000)).toISOString();
+        let day3 = (new Date(Date.now() - 2 * 86400000)).toISOString();
+        let day4 = (new Date(Date.now() - 3 * 86400000)).toISOString();
+        let day5 = (new Date(Date.now() - 4 * 86400000)).toISOString();
+
+
+        let result = {};
+        result[day1] = +(f[day] * hour / 24).toFixed(1);
+        result[day2] = f[day + 1];
+        result[day3] = f[day + 2];
+        result[day4] = f[day + 3];
+        result[day5] = f[day + 4];
+
+        let foo = {
+            result: 0,
+            data: JSON.parse(JSON.stringify(result))
+        };
+        res.send(
+            JSON.stringify(foo)
+        );
+
+    }
+}
+
+module.exports = new getOperating();
